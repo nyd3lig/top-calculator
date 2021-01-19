0214e7a5c935435600f52deeaec1d09dd3785d95
Dines <Nyd3lig@gmail.com>
2021-01-19 11:04:39 +0100

    added a function sumContinued, which checks if the operators are chained

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let operator;
+let number1;
+let number2;
+let storage = [];
+
+
+
+let displayCalc = document.getElementById("display");
+let plusBtn = document.getElementById("plus");
+let divideBtn = document.getElementById("divide");
+let minusBtn = document.getElementById("minus");
+let multiplyBtn = document.getElementById("multiply");
+let clearBtn = document.getElementById("clear");
+let equalBtn = document.getElementById("equal");
+
+
+
+
 function add (num1, num2){
     return num1 + num2;
 }
@@ -17,21 +35,54 @@ function multiply (num1, num2){
 function operate(operator, num1, num2){
     return operator(num1, num2);
 }
-let operator;
-let number1;
-let number2;
-let storage = [];
-let numberStorage = [];
+
+function display(e){
+    if(operator === add || operator === sub || operator === divide || operator === multiply || number1 === "string") {
+    storage.push(e.target.value)
+    number2 = storage.join("");
+    displayCalc.innerHTML = storage.join("");
+    console.log("this is the display if statement and number1: " + typeof number1 + "and number2: " + typeof number2)
+    return +number2;
+
+    console.log("this is the display if statement and number1: " + number1 + " and number2: " + number2)
+   
+    } 
+    storage.push(e.target.value)
+    number1 = storage.join("");
+    displayCalc.innerHTML = storage.join("");
+    return +number1;
+    }
+
+function sum(){
+    let result = operate(operator, +number1, +number2);
+    displayCalc.innerHTML = result;
+    number1 = result.toString();
+    number2 = null;
+    return result
+    
+}
+
+function sumContinued(){
+    let result = operate(operator,+number1, +number2);
+    let resultString = result.toString;
+    console.log(resultString.length)
+    if (resultString.length > 9){
+        console.log("this is a long ass number")
+        result = result.toFixed(6);
+    }
+    displayCalc.innerHTML = result;
+    
+    number1 = result.toString();
+    return result
+}
+
+function checkIfContinuedOperations(){
+    if(typeof number1 === "string" && typeof number2 === "string"){
+        sumContinued();
+    }
+}
 
 
-
-let displayCalc = document.getElementById("display");
-let plusBtn = document.getElementById("plus");
-let divideBtn = document.getElementById("divide");
-let minusBtn = document.getElementById("minus");
-let multiplyBtn = document.getElementById("multiply");
-let clearBtn = document.getElementById("clear");
-let equalBtn = document.getElementById("equal");
 
 
 
@@ -41,27 +92,25 @@ document.querySelectorAll(".buttonNumber").forEach(item => {
 })
 
 plusBtn.addEventListener("click", function(){
-    console.log(number1);
-console.log(number2);
-    if(number2 == "string"){
-        console.log("both numbers are string")
-       sum();
-    }
+    checkIfContinuedOperations();
     storage = []
     operator = add;
 })
 
 minusBtn.addEventListener("click", function(){
+    checkIfContinuedOperations();
     storage = [];
     operator = sub;
 })
 
 divideBtn.addEventListener("click", function(){
+    checkIfContinuedOperations();
     storage = [];
     operator = divide;
 })
 
 multiplyBtn.addEventListener("click", function(){
+    checkIfContinuedOperations();
     storage = [];
     operator = multiply;
 })
@@ -72,32 +121,15 @@ clearBtn.addEventListener("click", function(e){
     operator = null;
     storage = [];
     displayCalc.innerHTML = storage;
+    number1 = null;
+    number2 = null;
+    console.log(number1)
+    console.log(number2)
+    console.log(operator)
 })
 
 equalBtn.addEventListener("click", sum)
 
 
-function display(e){
-    if(operator === add || operator === sub || operator === divide || operator === multiply || number1 === "string") {
-    storage.push(e.target.value)
-    number2 = storage.join("");
-    displayCalc.innerHTML = storage.join("");
-    console.log("this is n2 " + typeof number2)
-    return +number2;
-   
-    }
-    storage.push(e.target.value)
-    number1 = storage.join("");
-    displayCalc.innerHTML = storage.join("");
-    console.log("this is n1 " + typeof number1)
-    return +number1;
-    }
-
-function sum(){
-    let result = operate(operator,+number1, +number2);
-    displayCalc.innerHTML = result;
-    number1 = result;
-    return result
-}
 
 
